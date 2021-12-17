53258e80cf97b4d4cf7eb6a626af2c5e73cfdc76
Konstantin Rohde <konstantin.rohde@bayes.gg>
2021-12-17 11:07:48 +0100

    Added some day 17 comments.

--- puzzles/day_17/solution_part_2.py
+++ puzzles/day_17/solution_part_2.py
@@ -27,16 +27,21 @@ def calculate_solution(hor: Tuple[int, int], vert: Tuple[int, int]) -> int:
                     continue
 
                 if in_horizontal_fadeout_range is True:
+                    # On fadeout - horizontal velocity is fixed
                     can_be_reached_in_n_steps_horizontally = True
                 else:
+                    # Check if is overshot when targetted with fadeout velocity.
                     steps_fadeout = (n_steps * (n_steps + 1)) // 2
 
                     if i == steps_fadeout:
+                        # Hits the horizontal position on fadeout
                         in_horizontal_fadeout_range = True
                         fadeout_velocity = n_steps
                     elif steps_fadeout > i:
+                        # Overshoots on fadeout - won't reach ever.
                         in_horizontal_fadeout_range = False
 
+                    # Direct calculation of capability to be reached
                     if n_steps % 2 == 1:
                         can_be_reached_in_n_steps_horizontally = i % n_steps == 0
                     else:
@@ -45,6 +50,7 @@ def calculate_solution(hor: Tuple[int, int], vert: Tuple[int, int]) -> int:
                 if not can_be_reached_in_n_steps_horizontally:
                     continue
 
+                # Direct calculation of capability to be reached
                 if n_steps % 2 == 1:
                     can_be_reached_in_n_steps_vertically = j % n_steps == 0
                 else:
@@ -54,12 +60,16 @@ def calculate_solution(hor: Tuple[int, int], vert: Tuple[int, int]) -> int:
                     continue
 
                 if in_horizontal_fadeout_range:
+                    # In case horizontal velocity is fixed - no calculation
                     init_velo_x = fadeout_velocity
                 else:
+                    # Calculation based on step count
                     init_velo_x = (i + (n_steps * n_steps - 1) // 2) // n_steps
 
+                # Calculation based on step count
                 init_velo_y = (j + (n_steps * n_steps - 1) // 2) // n_steps
 
+                # Add velocity vector to set - note that one velocity vector can hit the target in multiple step counts.
                 velocity_vectors.add((init_velo_x, init_velo_y))
 
     return len(velocity_vectors)
