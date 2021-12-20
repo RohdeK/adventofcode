683b278822a078d0d4a195a04cf6bd25108d66da
Konstantin Rohde <konstantin.rohde@bayes.gg>
2021-12-20 16:06:08 +0100

    Day 20 part 2

---
+++ puzzles/day_20/solution_part_2.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from puzzles.day_20.input_part_1 import get_input
+from puzzles.day_20.solution_part_1 import enhance_pixels
+
+
+def calculate_solution(input_pixels: List[List[bool]], input_enhancement: List[bool]) -> int:
+    pixel_field = {(x, y): pixel for y, line in enumerate(input_pixels) for x, pixel in enumerate(line)}
+
+    for i in range(50):
+
+        pixel_field = enhance_pixels(pixel_field, input_enhancement, call_even=i % 2 == 1)
+
+    return sum(pixel_field.values())
+
+
+if __name__ == "__main__":
+    enhancement, pixels = get_input()
+    print(calculate_solution(pixels, enhancement))
