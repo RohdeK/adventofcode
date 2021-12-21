ebd1070798be56621abc8295328f54b0328edbad
Konstantin Rohde <konstantin.rohde@bayes.gg>
2021-12-21 11:11:36 +0100

    Somewhat faulty but better approach

--- puzzles/day_21/solution_part_2.py
+++ puzzles/day_21/solution_part_2.py
@@ -1,4 +1,5 @@
-from typing import List
+from collections import defaultdict
+from typing import Dict, List
 
 from puzzles.day_21.input_part_1 import get_input
 from puzzles.day_21.solution_part_1 import DiracGame, Die
@@ -29,16 +30,51 @@ class DieFactory:
 
 
 def calculate_solution(input_values: List[int]) -> int:
-    wins = [0] * len(input_values)
-    fac = DieFactory(win_score=21, player_count=len(input_values))
+    die_faces = (1, 2, 3)
 
-    for die in fac:
+    triple_cast = defaultdict(int)
+    for i in die_faces:
+        for j in die_faces:
+            for k in die_faces:
+                triple_cast[i + j + k] += 1
 
-        game = DiracGame(starting_pos=input_values, max_board_num=10, win_score=21, die_used=die)
+    def iterative_roll(
+        combination_dict: Dict[int, int],
+        previous_rolls: List[int],
+        iter_pos: int,
+        iter_score: int,
+        iter_combinations: int,
+    ):
+        for triple_roll, combinations in triple_cast.items():
 
-        wins[game.play_out()] += 1
+            next_pos = (iter_pos + triple_roll) % 10 or 10
+            next_score = iter_score + iter_pos
+            next_combinations = iter_combinations * combinations
 
-    return max(wins)
+            if next_score >= 21:
+                combination_dict[len(previous_rolls + [triple_roll])] += next_combinations
+            else:
+                iterative_roll(
+                    combination_dict, previous_rolls + [triple_roll], next_pos, next_score, next_combinations
+                )
+
+    player_1_combinations_wins = defaultdict(int)
+    player_2_combinations_wins = defaultdict(int)
+
+    iterative_roll(player_1_combinations_wins, [], input_values[0], 0, 1)
+    iterative_roll(player_2_combinations_wins, [], input_values[1], 0, 1)
+
+    combinations_player_1_wins_over_2 = 0
+    combinations_player_2_wins_over_1 = 0
+
+    for roundnr_1, combinations_1 in player_1_combinations_wins.items():
+        for roundnr_2, combinations_2 in player_2_combinations_wins.items():
+            if roundnr_1 <= roundnr_2:
+                combinations_player_1_wins_over_2 += combinations_1 * combinations_2
+            else:
+                combinations_player_2_wins_over_1 += combinations_1 * combinations_2
+
+    return max(combinations_player_1_wins_over_2, combinations_player_2_wins_over_1)
 
 
 if __name__ == "__main__":
