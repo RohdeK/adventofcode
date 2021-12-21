f2c728b0381745a8a310811f1c7fb0cdcf236f11
Konstantin Rohde <konstantin.rohde@bayes.gg>
2021-12-21 15:48:15 +0100

    Day 21 part 2 (possibility spaces)

--- puzzles/day_21/solution_part_2.py
+++ puzzles/day_21/solution_part_2.py
@@ -1,80 +1,90 @@
 from collections import defaultdict
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from puzzles.day_21.input_part_1 import get_input
-from puzzles.day_21.solution_part_1 import DiracGame, Die
 
 
-class DiracDie(Die):
-    def __init__(self, seed: int):
-        super().__init__()
-        self.profile = f"{seed:3d}"
+class PossibilitySpaceDiracGame:
+    def __init__(self, starting_pos: Tuple[int, int], max_board_num: int, die_faces: int, win_score: int):
+        self.current_state_space: Dict[Tuple[int, int, int, int], int] = defaultdict(int)
+        self.current_state_space[(starting_pos[0], starting_pos[1], 0, 0)] = 1
 
-    def _next_roll_result(self) -> int:
-        if self.rolls > len(self.profile):
-            return 1
+        self.board_size = max_board_num
+        self.win_score = win_score
 
-        return int(self.profile[-1]) + 1
+        self.triple_die_result_space: Dict[int, int] = defaultdict(int)
+        for i in range(1, die_faces + 1):
+            for j in range(1, die_faces + 1):
+                for k in range(1, die_faces + 1):
+                    self.triple_die_result_space[i + j + k] += 1
 
+        self.player_1_wins = 0
+        self.player_2_wins = 0
 
-class DieFactory:
-    def __init__(self, win_score: int, player_count: int):
-        self.die_threshold = 3 ** (win_score * player_count * 3)
-        self.dies_produced = 0
+    def round(self) -> None:
+        after_1_positioning: Dict[Tuple[int, int, int, int], int] = defaultdict(int)
 
-    def __iter__(self):
-        while self.dies_produced < self.die_threshold:
-            self.dies_produced += 1
+        for state, state_frequency in self.current_state_space.items():
+            position_1, position_2, score_1, score_2 = state
 
-            yield DiracDie(self.dies_produced)
+            for roll, roll_frequency in self.triple_die_result_space.items():
+
+                position_1_after_roll = position_1 + roll
+                position_1_after_roll = position_1_after_roll % self.board_size or self.board_size
+
+                score_1_after_roll = score_1 + position_1_after_roll
+
+                after_1_positioning[(position_1_after_roll, position_2, score_1_after_roll, score_2)] += (
+                    state_frequency * roll_frequency
+                )
+
+        for state, state_frequency in tuple(after_1_positioning.items()):
+            _, _, score_1, _ = state
+
+            if score_1 >= self.win_score:
+                self.player_1_wins += state_frequency
+
+                del after_1_positioning[state]
+
+        after_2_positioning: Dict[Tuple[int, int, int, int], int] = defaultdict(int)
+
+        for state, state_frequency in after_1_positioning.items():
+            position_1, position_2, score_1, score_2 = state
+
+            for roll, roll_frequency in self.triple_die_result_space.items():
+
+                position_2_after_roll = position_2 + roll
+                position_2_after_roll = position_2_after_roll % self.board_size or self.board_size
+
+                score_2_after_roll = score_2 + position_2_after_roll
+
+                after_2_positioning[(position_1, position_2_after_roll, score_1, score_2_after_roll)] += (
+                    state_frequency * roll_frequency
+                )
+
+        for state, state_frequency in tuple(after_2_positioning.items()):
+            _, _, _, score_2 = state
+
+            if score_2 >= self.win_score:
+                self.player_2_wins += state_frequency
+
+                del after_2_positioning[state]
+
+        self.current_state_space = after_2_positioning
+
+    def play_out(self) -> None:
+        while self.current_state_space:
+            self.round()
 
 
 def calculate_solution(input_values: List[int]) -> int:
-    die_faces = (1, 2, 3)
+    game = PossibilitySpaceDiracGame(
+        starting_pos=(input_values[0], input_values[1]), max_board_num=10, die_faces=3, win_score=21
+    )
 
-    triple_cast = defaultdict(int)
-    for i in die_faces:
-        for j in die_faces:
-            for k in die_faces:
-                triple_cast[i + j + k] += 1
+    game.play_out()
 
-    def iterative_roll(
-        combination_dict: Dict[int, int],
-        previous_rolls: List[int],
-        iter_pos: int,
-        iter_score: int,
-        iter_combinations: int,
-    ):
-        for triple_roll, combinations in triple_cast.items():
-
-            next_pos = (iter_pos + triple_roll) % 10 or 10
-            next_score = iter_score + iter_pos
-            next_combinations = iter_combinations * combinations
-
-            if next_score >= 21:
-                combination_dict[len(previous_rolls + [triple_roll])] += next_combinations
-            else:
-                iterative_roll(
-                    combination_dict, previous_rolls + [triple_roll], next_pos, next_score, next_combinations
-                )
-
-    player_1_combinations_wins = defaultdict(int)
-    player_2_combinations_wins = defaultdict(int)
-
-    iterative_roll(player_1_combinations_wins, [], input_values[0], 0, 1)
-    iterative_roll(player_2_combinations_wins, [], input_values[1], 0, 1)
-
-    combinations_player_1_wins_over_2 = 0
-    combinations_player_2_wins_over_1 = 0
-
-    for roundnr_1, combinations_1 in player_1_combinations_wins.items():
-        for roundnr_2, combinations_2 in player_2_combinations_wins.items():
-            if roundnr_1 <= roundnr_2:
-                combinations_player_1_wins_over_2 += combinations_1 * combinations_2
-            else:
-                combinations_player_2_wins_over_1 += combinations_1 * combinations_2
-
-    return max(combinations_player_1_wins_over_2, combinations_player_2_wins_over_1)
+    return max(game.player_1_wins, game.player_2_wins)
 
 
 if __name__ == "__main__":
