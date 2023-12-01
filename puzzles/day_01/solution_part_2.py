777efd14995c0ff2d55a37c934988d27089724d2
RohdeK <konstantin.rohde@hellofresh.de>
2023-12-01 10:10:54 +0100

    Day 1 solution 2 (incorrect but working)

---
+++ puzzles/day_01/solution_part_2.py
@@ -0,0 +1,32 @@
+from puzzles.day_01.load_inputs import input_reader, InputType
+from puzzles.day_01.solution_part_1 import calculate_solution as calculate_solution_1
+
+DIGITS = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+
+def calculate_solution(input_values: InputType) -> int:
+    corrected_lines = []
+
+    for line in input_values:
+        for spelled, number in DIGITS.items():
+            line = line.replace(spelled, spelled + number + spelled)
+
+        corrected_lines.append(line)
+
+    return calculate_solution_1(corrected_lines)
+
+
+if __name__ == "__main__":
+    puzzle_input = input_reader.from_file("./input.txt")
+    print(calculate_solution(puzzle_input))
---
+++ puzzles/day_01/tests/test_solution_part_2.py
@@ -0,0 +1,20 @@
+from puzzles.day_01.load_inputs import input_reader
+from puzzles.day_01.solution_part_2 import calculate_solution
+
+
+def test_example():
+    raw_test_input = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
+    test_input = input_reader.load(raw_test_input)
+
+    solution = calculate_solution(test_input)
+
+    assert solution == 281
