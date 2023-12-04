e1455ebf19a6edb8bd980bf35beb7bcf19a36685
RohdeK <konstantin.rohde@hellofresh.de>
2023-12-04 08:47:33 +0100

    Day 4 solution 2 oneshot

---
+++ puzzles/day_04/solution_part_2.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from typing import Dict
+
+from puzzles.day_04.load_inputs import input_reader, InputType
+
+
+def calculate_solution(input_values: InputType) -> int:
+    card_copies: Dict[int, int] = defaultdict(lambda: 0)
+
+    for card_index, (winning_nums, elf_nums) in enumerate(input_values):
+        matches = [n for n in elf_nums if n in winning_nums]
+
+        for i in range(card_index + 1, card_index + len(matches) + 1):
+            card_copies[i] += 1 + card_copies[card_index]
+
+    return len(input_values) + sum(card_copies.values())
+
+
+if __name__ == "__main__":
+    puzzle_input = input_reader.from_file("./input.txt")
+    print(calculate_solution(puzzle_input))
