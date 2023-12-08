344e6be459f51c1efa404cd6043167b45316608f
RohdeK <konstantin.rohde@hellofresh.de>
2023-12-08 10:40:53 +0100

    Day 8 part 2 naive

--- puzzles/day_08/solution_part_2.py
+++ puzzles/day_08/solution_part_2.py
@@ -6,13 +6,16 @@ from puzzles.day_08.load_inputs import Locator, input_reader, InputType
 def calculate_solution(input_values: InputType) -> int:
     (move_pattern, ), locators = input_values
     locators_by_from: Dict[str, Locator] = {lc.from_: lc for lc in locators}
-    current_location = "AAA"
+
+    current_locations = [lc.from_ for lc in locators if lc.from_.endswith("A")]
     current_iteration = 0
 
-    while current_location != "ZZZ":
-        loc = locators_by_from[current_location]
-        direction = {"L": 0, "R": 1}[move_pattern[current_iteration % len(move_pattern)]]
-        current_location = [loc.left_, loc.right_][direction]
+    while not all(lc.endswith("Z") for lc in current_locations):
+        for i, lc in enumerate(current_locations):
+            loc = locators_by_from[lc]
+            direction = {"L": 0, "R": 1}[move_pattern[current_iteration % len(move_pattern)]]
+            current_locations[i] = [loc.left_, loc.right_][direction]
+
         current_iteration += 1
 
     return current_iteration
---
+++ puzzles/day_08/tests/test_solution_part_2.py
@@ -0,0 +1,23 @@
+from puzzles.day_08.load_inputs import input_reader
+from puzzles.day_08.solution_part_2 import calculate_solution
+
+
+def test_example():
+    raw_test_input = """
+    LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)
+    """
+    
+    test_input = input_reader.load(raw_test_input)
+
+    solution = calculate_solution(test_input)
+
+    assert solution == 6
